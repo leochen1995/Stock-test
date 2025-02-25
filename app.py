aab476868cfe6a005568718d643a29fd6e4eba9f
LeoChen <leo.chen1995@outlook.com>
2025-02-25 18:33:53 +0800

    Initial project version

---
+++ app.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import requests
+import json
+import datetime
+
+# 設定網址
+url = "https://www.tpex.org.tw/www/zh-tw/mostActive/brokerVol?id=&response=json"
+
+# 發送 GET 請求
+response = requests.get(url)
+
+# 檢查請求是否成功
+if response.status_code == 200:
+    # 解析 JSON 數據
+    data = response.json()
+    # 打印數據
+    print(json.dumps(data, indent=4, ensure_ascii=False))
+    json_data = data
+
+    # 加入新的值到 fields
+    json_data["tables"][0]["fields"].append("剩餘量(張)")
+
+else:
+    print(f"請求失敗，狀態碼：{response.status_code}")
+
+# 儲存到檔案
+with open('modified_data.json', 'w', encoding='utf-8') as f:
+    json.dump(json_data, f, ensure_ascii=False, indent=4)
+
+print("已儲存修改後的 JSON 檔案到 'modified_data.json'")
+
+# 讀取 JSON 檔案
+with open('modified_data.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+# 處理每個 data 項目
+for table in data['tables']:
+    for item in table['data']:
+        # 提取總買量和總賣量
+        total_buy = int(item[3])  # 總買量
+        total_sell = int(item[4])  # 總賣量
+        # 計算差異
+        difference = total_buy - total_sell
+        # 將結果儲存到最後一個位置，轉換為字串
+        item.append(str(difference))  # 轉換為字串以加上引號
+
+# 將修改後的資料寫入新的 JSON 檔案
+with open('modified_data.json', 'w', encoding='utf-8') as file:
+    json.dump(data, file, ensure_ascii=False, indent=4)
+
+print("已成功更新 modified_data.json 檔案。")
+
+# 將 data 寫入 CSV 檔案
+csv_data = []
+
+# 收集表頭
+headers = data['tables'][0]['fields']
+
+# 收集每一行的數據
+for table in data['tables']:
+    for item in table['data']:
+        csv_data.append(item)
+
+
+# 設定時間
+today = datetime.date.today()
+
+# 使用 pandas 將資料寫入 CSV
+df = pd.DataFrame(csv_data, columns=headers)
+df.to_csv(f'output_{today}_data.csv', index=False, encoding='utf-8')
+
+print(f"已成功將數據寫入 'output_{today}_data.csv' 檔案。")
+
+# 將資料寫入 XLSX 檔案
+df.to_excel(f'output_{today}_data.xlsx', index=False)
+
+print(f"已成功將數據寫入 'output_{today}_data.xlsx' 檔案。")
